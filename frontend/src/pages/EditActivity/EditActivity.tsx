0f58531a76f5feca0c4c379346a1407a665439da
Jean alomia <jeanalomia2828@gmail.com>
2026-05-27 19:05:47 -0500

    feat: create EditActivity page skeleton

---
+++ frontend/src/pages/EditActivity/EditActivity.tsx
@@ -0,0 +1,11 @@
+import PageHeader from '../../components/PageHeader/PageHeader';
+
+const EditActivity = () => {
+  return (
+    <div>
+      <PageHeader title="Edit Activity" subtitle="Update your plan." />
+    </div>
+  );
+};
+
+export default EditActivity;
